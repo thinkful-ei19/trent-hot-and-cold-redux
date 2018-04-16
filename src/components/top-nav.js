99b4b1b91752660fcfcba76f4e71629a25c50c08
trentduncan <trent.dunc@gmail.com>
2018-04-16 11:46:47 -0700

    refactored header to dispatch restart game action

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import './top-nav.css';
+import { connect } from 'react-redux';
+import {restartGame} from '../actions/index';
 
-export default function TopNav(props) {
+function TopNav(props) {
   return (
     <nav>
       <ul className="clearfix">
@@ -20,23 +22,23 @@ export default function TopNav(props) {
             href="#feedback"
             className="new"
             aria-label="Start a new game"
-            onClick={() => props.onRestartGame()}
+            onClick={() => props.dispatch(restartGame())}
           >
             + New Game
           </a>
         </li>
         <li>
-          <a
+          {/* <a
             href="#get-status"
-            /* the `visuallyhidden` class hides an element 
-            while leaving it available to screen reader users  */
             className="visuallyhidden focusable status-link"
             onClick={() => props.onGenerateAuralUpdate()}
           >
-            Hear state of game
-          </a>
+            Hear state of game 
+          </a>*/}
         </li>
       </ul>
     </nav>
   );
 }
+
+export default connect()(TopNav);
--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ import guessReducer from './reducers'
 
 
 
-const store = createStore(guessReducer);
+const store = createStore(guessReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
